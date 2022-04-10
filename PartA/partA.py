01765d200cac143d799860e8744d584276a8aa35
kumarmanishiitm <99637253+kumarmanishiitm@users.noreply.github.com>
2022-04-10 16:25:28 +0530

    Add files via upload

--- PartA/partA.py
+++ PartA/partA.py
@@ -159,17 +159,11 @@ if __name__ == "__main__":
     """
     README Part-A - Q1 --------------------------------------------------------------------
         
-        To compile the file with command line arguments write in following format in terminal :-
+      To compile the file with command line arguments write in following format in terminal :-
 
-        $ python filename.py nf1 nf2 nf3 nf4 nf5 filter_size batch_size dropout batch_normalisation(True/False) epochs dense_neurons learning_rate activation padding
-        
-          nfi = number of filters in ith Layer
-        =>nf3 = number of filters in 3rd layer
+    !python3 filename filter_multiplier,num_filters dropout dense_size  batch_normalisation
+    Example:
+     !python3 q1.py 2 32 0.27 1331 True True 1 0.0007
 
-        
-        Example:-
-        $ python A2_PartA_Q1.py 8 16 32 64 128 5 32 0.15 True 2 32 0.001 relu same
-
-        number of fileters in each convolution layer is flexible and can be given by user in command line.
     
     """
